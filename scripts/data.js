21046919c243e50aa9d5ce276bf49f5ba7e58091
mezue30000 <mezue3000>
2023-11-29 05:09:11 +0100

    Stored my data in array of objects

---
+++ scripts/data.js
@@ -0,0 +1,63 @@
+const services = [
+    {
+        Icon: '<i class="fa-brands fa-github">',
+        Title: 'Web Development',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-brands fa-codepen">',
+        Title: 'Web Design',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-solid fa-video">',
+        Title: 'Video Editing',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-solid fa-camera">',
+        Title: 'Photography',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-brands fa-apple">',
+        Title: 'App DEvelopment',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-brands fa-yoast">',
+        Title: 'SEO Expert',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: ' <i class="fa-solid fa-trophy"></i>',
+        Title: 'Net Award',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-brands fa-codepen"></i>',
+        Title: 'Full Stack',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+
+
+    {
+        Icon: '<i class="fa-solid fa-users"></i>',
+        Title: 'Cyber Security',
+        About: 'Lorem ipsum is simply dummy text of the printing typesetting industry.simply dummy'
+    },
+]
